1ed6b8bdb1b4fb0e1cf95c9272773aff36c2d602
Alik218 <Alik218@ukr.net>
2020-12-20 18:18:39 +0200

    dz21_12_2020

---
+++ 21_12_2020/js/main.js
@@ -0,0 +1,33 @@
+const btnLightEl = document.querySelector('.light2')
+const lightEl = document.querySelector('.light_on')
+const btnRightEl = document.querySelector('.right')
+const btnLeftEl = document.querySelector('.left')
+const trainEl = document.querySelector('.train')
+let state=true;
+
+
+btnLightEl.addEventListener('click', () => {
+    lightEl.classList.toggle('light_on')
+})
+
+btnRightEl.addEventListener('click' , () => {
+    document.getElementsByClassName('train')[0].style= "animation: voyage 10s linear infinite";
+    trainEl.style.animationPlayState=state?'paused':'running';
+    state=!state;
+})
+
+btnLeftEl.addEventListener('click' , () => {
+    document.getElementsByClassName('train')[0].style= "animation: voyage2 10s linear infinite";
+    trainEl.style.animationPlayState=state?'paused':'running';
+    state=!state;
+})
+
+btnLeftEl.addEventListener('click' , () => {
+    if (trainEl.classList.contains('train')){
+    trainEl.classList.remove('train')
+    trainEl.classList.add('train_left')}
+    else {
+    trainLeftEl.style.animationPlayState=state?'paused':'running';
+    state=!state;
+    }
+})
