f23bea04a197c862f1b00a091c7d7256a07128e9
Alik218 <Alik218@ukr.net>
2020-12-29 13:16:41 +0200

    metods

--- 19_12_2020_metods/main.js
+++ 19_12_2020_metods/main.js
@@ -16,7 +16,8 @@ console.log(arr.filter(value => value > 0 && value % 2 != 0));
 console.log(arr.reduce((sum, value) => sum + (value % 3), 0));
 
 // e.Проверить, и вывести в консоль результат проверки, есть ли в массиве число 5.
-console.log(arr.some(value => value === 5));
+const arrCheckNum = arr.includes(5);
+console.log(arrCheckNum)
 
 // f.Получить массив без первого элемента, вывести в консоль.
 console.log(arr.slice(1));
