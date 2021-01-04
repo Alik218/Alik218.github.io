b8f16cd15be09a67a8bc2fe3c1000fb0fc586676
Alik218 <Alik218@ukr.net>
2021-01-04 19:53:47 +0200

    pra_04_01_2021

---
+++ 04_01_2021_light/js/main.js
@@ -0,0 +1,31 @@
+const btnRedEl = document.querySelector('.btn_red')
+const btnOrangEl = document.querySelector('.btn_orang')
+const btnGreenEl = document.querySelector('.btn_green')
+
+const lightsRedEl = document.querySelector('.red')
+const lightsOrangEl = document.querySelector('.orang')
+const lightsGreenEl = document.querySelector('.green')
+
+const offLights = () => {
+  lightsRedEl.classList.remove('red_on');
+  lightsOrangEl.classList.remove('orang_on');
+  lightsGreenEl.classList.remove('green_on');
+};
+
+btnRedEl.addEventListener('click', () => {
+  offLights()
+  lightsRedEl.classList.add('red_on')
+  })
+
+
+btnOrangEl.addEventListener('click', () => {
+  offLights()
+  lightsOrangEl.classList.add('orang_on')
+  })
+
+
+btnGreenEl.addEventListener('click', () => {
+  offLights()
+  lightsGreenEl.classList.add('green_on')
+  })
+}
