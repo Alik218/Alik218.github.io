60b908861206393c3f2d125d8dde2ec3f2971c99
Alik218 <Alik218@ukr.net>
2021-01-07 15:24:11 +0200

    parking

--- 07_01_2021_Parking/js/main.js
+++ 07_01_2021_Parking/js/main.js
@@ -1,4 +1,8 @@
 // дз 07_01_2021
+const barEl = document.querySelector('.bar')
+const mainEl = document.querySelector('.main')
+const timeEl = document.querySelector('.time')
+const parkingSlot = document.querySelectorAll('.parking-slot')
 
 // 1) Сгенерировать массив паркомест. Количество задано в константе PLACES. Каждое паркоместо имеет свойства: id, occupied, time
 const PLACES = 50
@@ -16,22 +20,19 @@ const generateParking = (parkingArr) => {
 }
 
 generateParking(parking)
-parking[1].occupied = true;
-parking[2].occupied = true;
 
 console.log (parking)
 
 // 2) Функция подсчета количества занятых и свободных паркомест
 
-const barEl = document.querySelector('.bar')
-
 const placeBusy = (parkingArr) => {
   count = 0 
   parkingArr.forEach(i => {
-    if(i.occupied) {count++}
+    if(i.occupied) count++
   })
   return count
 }
+
 
 placeBusy(parking)
 
@@ -47,37 +48,42 @@ barEl.innerHTML = `FREE: ${placeFree(parking)} , BUSY: ${placeBusy(parking)}`
 
 // 3) Отобразить на странице паркоместа с указанием id, свободно/занято, время занятого. 
 
-const mainEl = document.querySelector('.main')
-
-const genOneSpaceDiv = (index) => {
-  let str =''
-  str = `<div class="OneCar free" id="${index}">
-  Номер места: ${parking[index].id}<br>
-  Статус: ${parking[index].occupied}<br>
-  Время: ${parking[index].time}<br>
-  </div>`
-  return str
-}
-
-const generateParkingHTML = () => {
+const displayAllParkingStatus = (parkingArr) => {  /// Рисуем на странице статус парковок
   let str = ''
-  parking.forEach((value,index) => {
-      str = str + genOneSpaceDiv(index)
+  parkingArr.forEach(element => {
+      str += `<div class="parking-slot free" id='${element.id}'>
+      Номер места: ${element.id}</br> 
+      Статус: ${element.occupied}</br>
+      Время: ${element.time}</div>`
   })
-  return str
+  mainEl.innerHTML = str
 }
 
-mainEl.innerHTML = generateParkingHTML()
+displayAllParkingStatus(parking)
 
 // 4) Также на странице отобразить текущее время
 
-const timeEl = document.querySelector('.time')
-
-const displayCurrentTime = () => {  /// Выводим на страницу текущее время и количество свободных мест
+const displayCurrentTime = () => {
   setInterval(() => {
       return timeEl.innerHTML = `Time: ${new Date().toLocaleTimeString()}`
   }, 1000)
 }
 displayCurrentTime()
 
-// 5) При нажатии на паркоместо, если оно пустое, появляется модальное окно с полем input, в нем текущее время, которое можно исправить, это время занимания парковки. При нажатии "ОК" парковка занимается.
+// 5) При нажатии на паркоместо, если оно пустое, появляется модальное окно с полем input, в нем текущее время, которое можно исправить, это время занимания парковки. При нажатии "ОК" парковка занимается. 
+
+
+
+
+
+// 5) При нажатии на паркоместо, если оно пустое, появляется модальное окно с полем input, в нем текущее время, которое можно исправить, это время занимания парковки. При нажатии "ОК" парковка занимается. 
+// 6) Если нажать на занятой парковке, то появляется модальное окно "освободить парковку? Да/Нет. Время занимания паркоместа: столько-то".
+// 7) При нажатии на пустой парковке, если количество свободных паркомест меньше 20% от всех и при этом время занимания больше 9:00 и меньше 18:00, то в модальном окне порекомендовать не занимать паркоместо.
+
+
+
+
+
+
+
+displayAllParkingStatus(parking)
