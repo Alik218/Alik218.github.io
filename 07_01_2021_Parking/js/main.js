885501f62b0b318d4767027c8220ebc5f35e220d
Alik218 <Alik218@ukr.net>
2021-01-07 18:01:06 +0200

    parking

--- 07_01_2021_Parking/js/main.js
+++ 07_01_2021_Parking/js/main.js
@@ -2,7 +2,13 @@
 const barEl = document.querySelector('.bar')
 const mainEl = document.querySelector('.main')
 const timeEl = document.querySelector('.time')
-const parkingSlot = document.querySelectorAll('.parking-slot')
+const inputFree = document.querySelector('.input-free-wrap')
+const inputOcupied = document.querySelector('.input-ocupied')
+const inputTime = document.querySelector('.input')
+const btnInput = document.querySelector('.btn-input')
+const btnOk = document.querySelector('.btn-ok')
+const btnCancel = document.querySelector('.btn-no')
+const parkingTime = document.querySelector('.parking-time')
 
 // 1) Сгенерировать массив паркомест. Количество задано в константе PLACES. Каждое паркоместо имеет свойства: id, occupied, time
 const PLACES = 50
@@ -50,7 +56,7 @@ const parkingPlase = () => {
 const displayAllParkingStatus = (parkingArr) => {  /// Рисуем на странице статус парковок
   let str = ''
   parkingArr.forEach(element => {
-      str += `<div class="parking-slot free" id='${element.id}'>
+      str += `<div class="parking free" id='${element.id}'>
       Номер места: ${element.id}</br> 
       Статус: ${element.occupied}</br>
       Время: ${element.time}</div>`
@@ -71,29 +77,68 @@ displayCurrentTime()
 
 // 5) При нажатии на паркоместо, если оно пустое, появляется модальное окно с полем input, в нем текущее время, которое можно исправить, это время занимания парковки. При нажатии "ОК" парковка занимается. 
 
-mainEl.addEventListener('click', (element) => {
-  parkingSlot.forEach(el => {
-    if(el.occupied == 'free') {
-      parking[el] = 'qwer'
-    } 
-  })
-  console.log(element)
+const checkParkingStatus = (element) => {  /// Проверяем свободна парковка или нет
+  return element.occupied === 'free' ? true : false;
+}
 
+displayAllParkingStatus(parking)
+const parkingAll = document.querySelectorAll('.parking')
+
+parkingAll.forEach (element => {
+  element.addEventListener('click', () => {
+    index = element.id
+    if (checkParkingStatus(parking[index])) {
+        inputOcupied.classList.remove('visible')
+        inputFree.classList.add('visible')
+
+    } else {
+        inputFree.classList.remove('visible')
+        inputOcupied.classList.add('visible')
+        parkingTime.innerHTML = `Оставшееся время: ${calcTimeLast()}`
+    }
+  })
 })
 
 
-parking[1].occupied = 'busy'
+const refreshStatus = () => { 
+  parkingAll[index].innerHTML = `Номер места: ${parking[index].id}</br> Статус: ${parking[index].occupied}</br>Время: ${parking[index].time}`
+}
 
+const parsTimeInput = () => {
+  let m = moment()
+  let inputHours = parking[index].time.slice(0, 2)
+  let inputMinutes = parking[index].time.slice(-1)
+  inputHours < moment().hour() ? m.add(1, 'd') : true;
+  let input = m.hours(inputHours).minutes(inputMinutes)
+  return input
+}
 
+const calcTimeLast = () => { 
+  let timeDiff = parsTimeInput() - moment() 
+  let timeLast = `${moment.utc(timeDiff).format('hh:mm')}`
+  return timeLast
+}
 
-// 6) Если нажать на занятой парковке, то появляется модальное окно "освободить парковку? Да/Нет. Время занимания паркоместа: столько-то".
-// 7) При нажатии на пустой парковке, если количество свободных паркомест меньше 20% от всех и при этом время занимания больше 9:00 и меньше 18:00, то в модальном окне порекомендовать не занимать паркоместо.
+btnInput.addEventListener('click', () => {
+  parking[index].occupied = 'busy'
+  parking[index].time = inputTime.value
+  parkingAll[index].classList.add('ocupied')
+  refreshStatus()
+  displayCurrentTime()
+  inputFree.classList.remove('visible')
+})
 
+btnOk.addEventListener('click', () => {
+  parking[index].occupied = 'free'
+  parking[index].time = 0
+  parkingAll[index].classList.remove('ocupied')
+  refreshStatus()
+  displayCurrentTime()
+  inputOcupied.classList.remove('visible')
+})
 
+btnCancel.addEventListener('click', () => { 
+  inputOcupied.classList.remove('visible')
+})
 
-
-
-
-
-displayAllParkingStatus(parking)
 parkingPlase()
