81c55e6c6f6ba0f6aafee0d381547048f5db1452
Alik218 <Alik218@ukr.net>
2021-05-25 18:53:57 +0300

    udemy

---
+++ udemy/21.05.2021_start/js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+
+//  // Урок 10 GitHub
+
+
+// // Урок 9 Операторы в JS
+
+
+// let incr = 10;
+// let decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+  
+
+// // Урок 8 Интерполяция (ES6)
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Vitaliy';
+
+// alert(`Hello, ${user}`);
+
+
+// // Урок 7 Простое общения с пользователем
+
+
+// alert('hello wold');
+
+// const result = confirm('Are you heree?');
+// console.log(result);
+
+// const answer = prompt('You heaw 18?','18');
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как ваше фамилия?', '');
+// answers[2] = prompt('лет скока а?', '');
+
+// document.write(answers);
+
+
